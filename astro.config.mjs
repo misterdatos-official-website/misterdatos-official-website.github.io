93750c5d02e1fc73c19662c1f2f450fc5eff6dd1
misterdatos-official-website <pluslelover@gmail.com>
2026-03-14 17:35:13 -0500

    PWA and Notifications added 14/03/2026 05:35 pm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,52 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import AstroPWA from '@vite-pwa/astro';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    AstroPWA({
+      mode: 'production',
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png', 'pwa-192x192.png', 'pwa-512x512.png'],
+      manifest: {
+        name: 'MisterDatos App',
+        short_name: 'MisterDatos',
+        description: 'Sitio Web Oficial de MisterDatos',
+        theme_color: '#1c0a1b',
+        background_color: '#ffffff',
+        start_url: '/',
+        scope: '/',
+        display: 'standalone',
+        orientation: 'portrait-primary',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          }
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 año
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+});
