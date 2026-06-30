ffef4fee4c8eebd758278cd37fe0705406f1e30b
misterdatos-official-website <pluslelover@gmail.com>
2026-06-30 04:57:50 -0500

    Eliminado OneSignal y PWA 30/06/26

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,57 +1,6 @@
 import { defineConfig } from 'astro/config';
-import AstroPWA from '@vite-pwa/astro';
 
 export default defineConfig({
   site: 'https://misterdatos-official-website.github.io/',
-  integrations: [
-    AstroPWA({
-      mode: 'production',
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.png', 'pwa-192x192.png', 'pwa-512x512.png'],
-      devOptions: {
-        enabled: false,
-        type: 'module'
-      },
-      manifest: {
-        name: 'MisterDatos App',
-        short_name: 'MisterDatos',
-        description: 'Sitio Web Oficial de MisterDatos',
-        theme_color: '#1c0a1b',
-        background_color: '#ffffff',
-        start_url: '/',
-        scope: '/',
-        display: 'standalone',
-        orientation: 'portrait-primary',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any'
-          }
-        ],
-      },
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 año
-              },
-            },
-          },
-        ],
-      },
-    }),
-  ],
+  integrations: [],
 });
