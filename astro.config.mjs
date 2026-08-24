a1a1ca6dbf472ebfa3e29dcad97d8dcda739c9c8
misterdatos-official-website <pluslelover@gmail.com>
2026-08-24 05:13:45 -0500

    24.08.2026 5:13

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,19 @@
 import { defineConfig } from 'astro/config';
+import rehypeExternalLinks from 'rehype-external-links'; // 1. Añade este import arriba
 
+// https://astro.build
 export default defineConfig({
   site: 'https://misterdatos-official-website.github.io/',
   integrations: [],
+  markdown: {                                            // 2. Añade toda esta sección de markdown
+    rehypePlugins: [
+      [
+        rehypeExternalLinks, 
+        { 
+          target: '_blank', 
+          rel: ['noopener', 'noreferrer'] 
+        }
+      ]
+    ],
+  },
 });
